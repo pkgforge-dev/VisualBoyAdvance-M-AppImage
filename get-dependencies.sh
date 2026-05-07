10c46ffc46420702c219ce418f8a22beeeb23298
Link4Electronics <link4electronics@gmail.com>
2026-05-07 07:49:33 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -6,21 +6,35 @@ ARCH=$(uname -m)
 
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
-# pacman -Syu --noconfirm PACKAGESHERE
+pacman -Syu --noconfirm \
+    cmake          \
+    openal         \
+    qt6-base       \
+    qt6-tools      \
+    sdl3           \
+    sfml           \
+    vulkan-headers \
+    zip
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
-get-debloated-pkgs --add-common --prefer-nano
+get-debloated-pkgs --add-common --prefer-nano ffmpeg-mini libdecor-mini x265-mini
 
-# Comment this out if you need an AUR package
-#make-aur-package PACKAGENAME
+echo "Building VisualBoyAdvance-M..."
+echo "---------------------------------------------------------------"
+REPO="https://github.com/visualboyadvance-m/visualboyadvance-m"
+VERSION="$(git ls-remote "$REPO" HEAD | cut -c 1-9 | head -1)"
+git clone --depth 1 "$REPO" ./visualboyadvance-m
+echo "$VERSION" > ~/version
 
-# If the application needs to be manually built that has to be done down here
-
-# if you also have to make nightly releases check for DEVEL_RELEASE = 1
-#
-# if [ "${DEVEL_RELEASE-}" = 1 ]; then
-# 	nightly build steps
-# else
-# 	regular build steps
-# fi
+cmake -B build -S ./visualboyadvance-m \
+      -DCMAKE_BUILD_TYPE=Release \
+      -DCMAKE_INSTALL_PREFIX=/usr \
+      -DCMAKE_INSTALL_SYSCONFDIR=/etc \
+      -DCMAKE_SKIP_RPATH=TRUE \
+      -DENABLE_QT=ON \
+      -DENABLE_WX=OFF \
+      -DENABLE_FFMPEG=TRUE \
+      -DBUILD_TESTING=OFF
+cmake --build build -j$(nproc)
+cmake --install build
