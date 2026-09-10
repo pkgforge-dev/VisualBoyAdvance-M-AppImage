139ff6a6ca84f9edb122b6f74d7fdef3d34e87e0
Link4Electronics <link4electronics@gmail.com>
2026-09-10 03:05:22 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -6,14 +6,21 @@ ARCH=$(uname -m)
 
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
-#pacman -Syu --noconfirm
+pacman -Syu --noconfirm \
+    cmake          \
+    gettext        \
+    libxss         \
+    openal         \
+    sdl3           \
+    sfml           \
+    wxwidgets-gtk3
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
 get-debloated-pkgs --add-common --prefer-nano ffmpeg-mini libdecor-mini
 
 # Comment this out if you need an AUR package
-PRE_BUILD_CMDS='sed -i "s/-Wno-dev)/-Wno-dev\\n              -DCMAKE_CXX_FLAGS=\\"-Wno-error=attributes\\")/" ./PKGBUILD' make-aur-package vbam-git
+#PRE_BUILD_CMDS='sed -i "s/-Wno-dev)/-Wno-dev\\n              -DCMAKE_CXX_FLAGS=\\"-Wno-error=attributes\\")/" ./PKGBUILD' make-aur-package vbam-git
 
 # If the application needs to be manually built that has to be done down here
 
@@ -24,3 +31,19 @@ PRE_BUILD_CMDS='sed -i "s/-Wno-dev)/-Wno-dev\\n              -DCMAKE_CXX_FLAGS=\
 # else
 # 	regular build steps
 # fi
+echo "Building Visual Boy Advance - M..."
+echo "---------------------------------------------------------------"
+REPO="https://github.com/visualboyadvance-m/visualboyadvance-m"
+VERSION="$(git ls-remote "$REPO" HEAD | cut -c 1-9 | head -1)"
+git clone --depth 1 "$REPO" ./visualboyadvance-m
+echo "$VERSION" > ~/version
+
+cmake -B build -S ./visualboyadvance-m \
+      -DCMAKE_BUILD_TYPE=Release \
+      -DCMAKE_INSTALL_PREFIX=/usr \
+      -DCMAKE_INSTALL_SYSCONFDIR=/etc \
+      -DCMAKE_SKIP_RPATH=TRUE \
+      -DENABLE_FFMPEG=TRUE \
+      -DBUILD_TESTING=OFF
+cmake --build build -j$(nproc)
+cmake --install build
