7cfac4e9c84760d5230f9322d74634696d03d416
Link4Electronics <link4electronics@gmail.com>
2026-09-11 20:16:36 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -8,8 +8,6 @@ echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
 pacman -Syu --noconfirm \
     cmake          \
-    gettext        \
-    libxss         \
     openal         \
     qt6-base       \
     sdl3           \
